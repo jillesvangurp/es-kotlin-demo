1ca25e3d3bf1aea916bd2c60db9e96868452fc3c
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2019-08-12 15:50:19 +0200

    use channels and co-routines

--- src/main/kotlin/io/inbot/eskotlindemo/5-simple-wikipedia-indexer.kt
+++ src/main/kotlin/io/inbot/eskotlindemo/5-simple-wikipedia-indexer.kt
@@ -6,6 +6,12 @@ import io.inbot.eskotlinwrapper.JacksonModelReaderAndWriter
 import io.inbot.xmltools.PooledXmlParser
 import io.inbot.xmltools.XPathExpressionCache
 import io.inbot.xmltools.XpathBrowserFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newFixedThreadPoolContext
+import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
 import org.elasticsearch.action.support.WriteRequest
@@ -13,6 +19,7 @@ import org.elasticsearch.client.RestHighLevelClient
 import org.elasticsearch.client.crudDao
 import org.elasticsearch.common.xcontent.XContentType
 import java.io.FileInputStream
+import java.util.concurrent.atomic.AtomicLong
 
 
 data class SimpleWikiPediaPage(
@@ -20,8 +27,10 @@ data class SimpleWikiPediaPage(
     val timestamp: String,
     val url: String,
     val title: String,
-    val description: String
+    val description: String,
+    val text: String
 )
+
 
 val logger = KotlinLogging.logger { }
 fun main() {
@@ -42,10 +51,10 @@ fun main() {
             source(this::class.java.getResource("/simple-wikpedia.json").readText(), XContentType.JSON)
         }
 
-        var count = 0
+//        var count = 0
         articleDao.bulk(
-            bulkSize = 300,
-            refreshPolicy = WriteRequest.RefreshPolicy.IMMEDIATE,
+            bulkSize = 500,
+            refreshPolicy = WriteRequest.RefreshPolicy.NONE,
             retryConflictingUpdates = 0
         ) {
             // our input
@@ -53,18 +62,66 @@ fun main() {
             val reader =
                 BZip2CompressorInputStream(FileInputStream(file)).reader()
 
-            BlobIterable(reader, "<page", "</page>").iterator()
-                .asSequence()
-                .take(1000) // whole thing would take a few minutes
-                .map { parsePage(xpbf, it) }
-                .filterNotNull() // filter out all the parser failures
-                .forEach {
-                    index(it.id.toString(), it)
-                    if (count % 100 == 0) {
-                        logger.info("processed $count articles")
-                    }
-                    count++
+            runBlocking {
+                val inputChannel = Channel<String?>()
+                val outputChannel = Channel<SimpleWikiPediaPage?>()
+                val pageReads = AtomicLong()
+                val processCount = AtomicLong()
+                val skipCount = AtomicLong()
+                val indexCount = AtomicLong()
+                launch {
+                    BlobIterable(reader, "<page", "</page>").iterator()
+                        .asSequence()
+//                        .take(10000) // whole thing would take a few minutes
+                        .forEach {
+                            inputChannel.send(it)
+                            if (pageReads.incrementAndGet() % 1000 == 0L) {
+                                logger.info("read ${pageReads.get()} articles")
+                            }
+                        }
+                    logger.info("DONE read ${pageReads.get()} articles")
+                    inputChannel.close()
                 }
+                launch(newFixedThreadPoolContext(6, "pageprocessor")) {
+                    for(page in inputChannel) {
+                        val processed = parsePage(xpbf, page)
+                        outputChannel.send(processed)
+                        if (processCount.incrementAndGet() % 1000 == 0L ) {
+                            logger.info("processed ${processCount.get()} articles")
+                        }
+
+                    }
+                    outputChannel.close()
+                    logger.info("DONE processed ${processCount.get()} articles")
+
+                }
+                launch {
+                    for(doc in outputChannel) {
+                        if (doc !=null) {
+                            index(doc.id.toString(), doc)
+                            if (indexCount.incrementAndGet() % 1000 == 0L) {
+                                logger.info("indexed ${indexCount.get()} articles; skipped ${skipCount.get()}")
+                            }
+                        } else {
+                            skipCount.incrementAndGet()
+                        }
+                    }
+                    logger.info("DONE indexed ${indexCount.get()} articles; skipped ${skipCount.get()}")
+                }
+            }
+
+//            BlobIterable(reader, "<page", "</page>").iterator()
+//                .asSequence()
+////                .take(10000) // whole thing would take a few minutes
+//                .map { parsePage(xpbf, it) }
+//                .filterNotNull() // filter out all the parser failures
+//                .forEach {
+//                    index(it.id.toString(), it)
+//                    if (count % 1000 == 0) {
+//                        logger.info("processed $count articles")
+//                    }
+//                    count++
+//                }
         }
     }
     logger.info("Done in ${(System.currentTimeMillis() - start) / 1000} seconds!")
@@ -95,6 +152,7 @@ private fun parsePage(
                     title = title,
                     url= "https://simple.wikipedia.org/wiki/${title.replace(" ","_")}",
                     description = description,
+                    text = text,
                     timestamp = timestamp
                 )
             } else {
